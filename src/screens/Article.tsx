5f143c3508e88898412c504559995762cb9afb69
a.kirillov <alexkirillof@yandex.ru>
2023-06-22 09:12:07 +0300

    add date

--- src/screens/Article.tsx
+++ src/screens/Article.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ArticleContent } from "../components/Article/ArticleContent";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ArticleContent} from '../components/Article/ArticleContent';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
-export const Article = ({ id, name }) => {
+export const Article = ({id, name}: {id: number; name: string}) => {
   const navigation = useNavigation();
   const route = useRoute();
   return (
     <SafeAreaView style={styles.container}>
-      <ArticleContent navigation={navigation} route={route} id={id} name={name}/>
+      <ArticleContent
+        navigation={navigation}
+        route={route}
+        id={id}
+        name={name}
+      />
     </SafeAreaView>
   );
 };
@@ -18,6 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor: "#cee8ed"
-  }
+    backgroundColor: '#cee8ed',
+  },
 });
